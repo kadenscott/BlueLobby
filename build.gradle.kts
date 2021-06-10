e9bb8c33663745dda97ee24faa4f5cde10d54418
Kaden Scott <kscottdev@gmail.com>
2021-06-09 21:49:32 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
+import net.kyori.indra.IndraCheckstylePlugin
+import net.kyori.indra.IndraPlugin
+import net.kyori.indra.IndraPublishingPlugin
+
+plugins {
+    id("net.kyori.indra") version "1.3.1"
+    id("net.kyori.indra.publishing") version "1.3.1"
+    id("net.kyori.indra.checkstyle") version "1.3.1"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "dev.kscott.bluelobby"
+version = "1.0.0"
+
+subprojects {
+    apply {
+        plugin<ShadowPlugin>()
+        plugin<IndraPlugin>()
+//        plugin<IndraCheckstylePlugin>()
+        plugin<IndraPublishingPlugin>()
+    }
+
+    repositories {
+        mavenCentral()
+
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.broccol.ai")
+    }
+
+    tasks {
+
+        indra {
+            gpl3OnlyLicense()
+
+            javaVersions {
+                target.set(16)
+            }
+        }
+
+        processResources {
+            expand("version" to rootProject.version)
+        }
+
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "bluelobby"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
