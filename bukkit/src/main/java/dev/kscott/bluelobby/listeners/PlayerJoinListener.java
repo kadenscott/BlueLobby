7afd711702f9504c8089bafb204b54ea611253d3
Kaden Scott <kscottdev@gmail.com>
2021-06-10 06:31:18 -0400

    delay MOTD for 2 seconds and play sound.

--- bukkit/src/main/java/dev/kscott/bluelobby/listeners/PlayerJoinListener.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/listeners/PlayerJoinListener.java
@@ -6,9 +6,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.yaml.snakeyaml.scanner.Constant;
 
+import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -17,19 +19,41 @@ import java.util.List;
 public class PlayerJoinListener implements Listener {
 
     /**
+     * The plugin's reference.
+     */
+    private final @NonNull JavaPlugin plugin;
+
+    /**
+     * Constructs {@code PlayerJoinListener}.
+     *
+     * @param plugin the plugin's reference
+     */
+    @Inject
+    public PlayerJoinListener(final @NonNull JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
      * Handles the player join event.
      *
      * @param event the event
      */
     @EventHandler
     public void handlePlayerJoin(final @NonNull PlayerJoinEvent event) {
-        final @NonNull Player player = event.getPlayer();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                final @NonNull Player player = event.getPlayer();
 
-        final @NonNull List<Component> motd = Constants.Chat.motd;
+                final @NonNull List<Component> motd = Constants.Chat.MOTD;
 
-        for (final @NonNull Component component : motd) {
-            player.sendMessage(component);
-        }
+                for (final @NonNull Component component : motd) {
+                    player.sendMessage(component);
+                }
+
+                Constants.Sounds.playSoundForPlayer(player, Constants.Sounds.DEFAULT_CHIME);
+            }
+        }.runTaskLater(this.plugin, 20 * 2);
     }
 
 }
