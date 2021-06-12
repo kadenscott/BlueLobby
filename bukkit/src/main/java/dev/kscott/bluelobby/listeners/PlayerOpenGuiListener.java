f5aed4980f4ff5365c51e36dc2af1876ad4d374d
Kaden Scott <kscottdev@gmail.com>
2021-06-12 08:45:16 -0400

    add PlayerOpenGuiListener

---
+++ bukkit/src/main/java/dev/kscott/bluelobby/listeners/PlayerOpenGuiListener.java
@@ -0,0 +1,25 @@
+package dev.kscott.bluelobby.listeners;
+
+import dev.kscott.bluelobby.menu.GameGui;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.StonecutterInventory;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Listens for player-related gui events.
+ */
+public class PlayerOpenGuiListener implements Listener {
+
+    @EventHandler
+    public void onOpenGui(final @NonNull InventoryOpenEvent event) {
+        final @NonNull Inventory inventory = event.getInventory();
+
+        if (inventory.getHolder() instanceof GameGui) {
+            final @NonNull StonecutterInventory stonecutter = (StonecutterInventory) inventory;
+        }
+    }
+
+}
