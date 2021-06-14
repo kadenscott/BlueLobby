4525e68642a18dd4a1ce56709c01c06e42eec9a9
Kaden Scott <kscottdev@gmail.com>
2021-06-13 20:55:35 -0400

    fix /games gui

--- bukkit/src/main/java/dev/kscott/bluelobby/menu/GameGui.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/menu/GameGui.java
@@ -3,38 +3,49 @@ package dev.kscott.bluelobby.menu;
 import broccolai.corn.adventure.AdventureItemBuilder;
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.StonecutterGui;
+import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import dev.kscott.bluelobby.utils.Constants;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.List;
 
 /**
  * The /games menu.
  */
-public class GameGui extends StonecutterGui {
+public class GameGui {
 
-    /**
-     * Constructs a new GUI.
-     */
-    public GameGui() {
-        super(LegacyComponentSerializer.legacySection().serialize(Component.text("mc.kaden.sh >> games").style(Constants.Chat.STYLE_HOVER)));
+    public static @NonNull Gui create() {
+        final @NonNull StonecutterGui gui = new StonecutterGui(LegacyComponentSerializer.legacySection().serialize(Component.text("mc.kaden.sh >> games").style(Constants.Chat.STYLE_DARK)));
 
-        this.getInputComponent().setItem(new GuiItem(
+        final @NonNull StaticPane pane = new StaticPane(0, 0, 1, 1);
+
+        pane.addItem(new GuiItem(
                 AdventureItemBuilder.adventure(Material.DARK_OAK_SIGN)
                         .name(Component.text()
                                 .append(Constants.Chat.SERVER_NAME)
                                 .append(Component.text(" games").color(Constants.Chat.COLOUR_LIGHT_BLUE))
+                                .decoration(TextDecoration.ITALIC, false)
                                 .asComponent()
                         )
                         .loreComponents(List.of(
                                 Component.text("Use this menu to select your a game!").style(Constants.Chat.STYLE_DEFAULT),
                                 Component.empty(),
                                 Component.text("Current active games:").style(Constants.Chat.STYLE_DEFAULT),
-                                Component.text("- Bonk").color(Constants.Chat.COLOUR_LIGHT_BLUE)
+                                Component.text("- Bonk").color(Constants.Chat.COLOUR_LIGHT_BLUE).decoration(TextDecoration.ITALIC, false)
                         ))
                         .build()
         ), 0, 0);
+
+        gui.getInputComponent().addPane(pane);
+
+        gui.setOnGlobalClick(event -> event.setCancelled(true));
+
+        return gui;
     }
 }
