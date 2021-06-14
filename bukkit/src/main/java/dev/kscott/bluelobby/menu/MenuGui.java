f89830d2a28c57beb9054dd9007f28f7319f694a
Kaden Scott <kscottdev@gmail.com>
2021-06-14 00:06:04 -0400

    add games item to MenuGui

--- bukkit/src/main/java/dev/kscott/bluelobby/menu/MenuGui.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/menu/MenuGui.java
@@ -1,6 +1,6 @@
 package dev.kscott.bluelobby.menu;
 
-import broccolai.corn.adventure.AdventureItemBuilder;
+import broccolai.corn.paper.PaperItemBuilder;
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
@@ -11,10 +11,10 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.checkerframework.checker.nullness.qual.NonNull;
-
-import java.util.List;
 
 /**
  * The /menu gui.
@@ -31,17 +31,35 @@ public class MenuGui {
 
         // Exit Item
         final @NonNull GuiItem exitItem = new GuiItem(
-                AdventureItemBuilder.adventure(Material.BARRIER)
+                PaperItemBuilder.paper(Material.BARRIER)
                         .name(Constants.Chat.SERVER_NAME)
-                        .loreComponents(Component.text("Click to go back").color(NamedTextColor.RED).decoration(TextDecoration.ITALIC, false))
+                        .loreComponents(Component.text("Click to go back")
+                                .color(NamedTextColor.RED)
+                                .decoration(TextDecoration.ITALIC, false))
                         .build(),
                 event -> event.getWhoClicked().closeInventory(InventoryCloseEvent.Reason.PLUGIN)
+        );
+
+        final @NonNull GuiItem gamesItem = new GuiItem(
+                PaperItemBuilder.paper(Material.DIAMOND)
+                        .name(Component.text("Games").decoration(TextDecoration.ITALIC, false))
+                        .loreComponents(Component.text("Click to play a game!")
+                                .color(NamedTextColor.BLUE)
+                                .decoration(TextDecoration.ITALIC, false))
+                        .build(),
+                event -> {
+                    final @NonNull Player player = (Player) event.getWhoClicked();
+
+                    player.closeInventory(InventoryCloseEvent.Reason.PLUGIN);
+                    GameGui.create().show(player);
+                }
         );
 
         // Pane
         final @NonNull StaticPane pane = new StaticPane(0, 0, 9, 1);
 
         pane.addItem(exitItem, 0, 0);
+        pane.addItem(gamesItem, 0, 4);
 
         gui.addPane(pane);
 
