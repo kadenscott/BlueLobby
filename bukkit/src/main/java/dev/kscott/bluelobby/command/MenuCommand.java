8523202b3077bff1f9f0198ed5d0b447c3cc7119
Kaden Scott <kscottdev@gmail.com>
2021-06-14 01:31:09 -0400

    open Gui on main thread

--- bukkit/src/main/java/dev/kscott/bluelobby/command/MenuCommand.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/command/MenuCommand.java
@@ -8,12 +8,31 @@ import dev.kscott.bluelobby.menu.GameGui;
 import dev.kscott.bluelobby.menu.MenuGui;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import javax.inject.Inject;
 
 /**
  * The /menu command class.
  */
-public class MenuCommand implements BaseCommand{
+public class MenuCommand implements BaseCommand {
+
+    /**
+     * JavaPlugin reference.
+     */
+    private final @NonNull JavaPlugin plugin;
+
+    /**
+     * Constructs {@code MenuCommand}.
+     *
+     * @param plugin the plugin reference
+     */
+    @Inject
+    public MenuCommand(final @NonNull JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     /**
      * Registers the /menu command.
@@ -33,14 +52,19 @@ public class MenuCommand implements BaseCommand{
      * @param context command context
      */
     private void handleMenuCommand(final @NonNull CommandContext<CommandSender> context) {
-        final @NonNull CommandSender sender = context.getSender();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                final @NonNull CommandSender sender = context.getSender();
 
-        if (sender instanceof Player player) {
-            final @NonNull Gui gui = MenuGui.create();
+                if (sender instanceof Player player) {
+                    final @NonNull Gui gui = MenuGui.create();
 
-            // TODO send opening message
+                    // TODO send opening message
 
-            gui.show(player);
-        }
+                    gui.show(player);
+                }
+            }
+        }.runTask(this.plugin);
     }
 }
