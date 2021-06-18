92540dcc4bcb9551bda410f66d9c1afe93fd7314
Kaden Scott <kscottdev@gmail.com>
2021-06-18 06:54:48 -0400

    add Game & TicTacToeGame

---
+++ bukkit/src/main/java/dev/kscott/bluelobby/games/Game.java
@@ -0,0 +1,9 @@
+package dev.kscott.bluelobby.games;
+
+public abstract class Game {
+
+    public abstract void enable();
+
+    public abstract void disable();
+
+}
---
+++ bukkit/src/main/java/dev/kscott/bluelobby/games/tic/tac/toe/TicTacToeGame.java
@@ -0,0 +1,16 @@
+package dev.kscott.bluelobby.games.tic.tac.toe;
+
+import dev.kscott.bluelobby.games.Game;
+
+public class TicTacToeGame extends Game {
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
