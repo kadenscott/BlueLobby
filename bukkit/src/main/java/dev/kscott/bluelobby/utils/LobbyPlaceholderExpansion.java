92197ce3c1c5ec2c07d9a4852413e7c23945154b
Kaden Scott <kscottdev@gmail.com>
2021-06-21 07:52:54 -0400

    add server/owner name placeholders

--- bukkit/src/main/java/dev/kscott/bluelobby/utils/LobbyPlaceholderExpansion.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/utils/LobbyPlaceholderExpansion.java
@@ -2,7 +2,9 @@ package dev.kscott.bluelobby.utils;
 
 import dev.kscott.bluelobby.LobbyPlugin;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
@@ -45,6 +47,28 @@ public class LobbyPlaceholderExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return null;
+        return "1.0.0";
+    }
+
+    /**
+     * Parses the placeholder and returns the requested value.
+     *
+     * @param player the player
+     * @param identifier the identifier
+     * @return the value
+     */
+    @Override
+    public @NonNull String onPlaceholderRequest(final @NonNull Player player, final @NonNull String identifier) {
+        return switch (identifier) {
+            case "server_name" -> {
+                yield LegacyComponentSerializer.legacySection().serialize(Constants.Chat.SERVER_NAME);
+            }
+            case "owner_name" -> {
+                yield LegacyComponentSerializer.legacySection().serialize(Constants.Chat.OWNER_NAME);
+            }
+            default -> {
+                yield "";
+            }
+        };
     }
 }
