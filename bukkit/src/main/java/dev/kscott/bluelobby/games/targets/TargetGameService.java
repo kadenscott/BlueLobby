05a2cfcd954288d27315a226f8cebf3967ab3db9
Kaden Scott <kscottdev@gmail.com>
2021-07-08 21:32:50 -0400

    updates to target game

--- bukkit/src/main/java/dev/kscott/bluelobby/games/targets/TargetGameService.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/games/targets/TargetGameService.java
@@ -5,24 +5,25 @@ import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import dev.kscott.bluelobby.area.LocationService;
 import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.event.NPCDamageByEntityEvent;
 import net.citizensnpcs.api.npc.MemoryNPCDataStore;
 import net.citizensnpcs.api.npc.MetadataStore;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.npc.NPCRegistry;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
-import java.lang.annotation.Target;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Manages the target game.
@@ -30,14 +31,20 @@ import java.util.UUID;
 @Singleton
 public class TargetGameService implements Listener {
 
-    private final @NonNull List<Location> targetLocations;
+    private static final @NonNull String REGION_ID = "area_targets";
+
     private final @NonNull JavaPlugin plugin;
+    private final @NonNull List<Location> targetLocations;
     private final @NonNull LocationService locationService;
     private final @NonNull World world;
     private final @NonNull NPCRegistry npcRegistry;
     private final @NonNull UUID gameUuid;
     private final @NonNull List<TargetInstance> targets;
+    private final @NonNull Set<UUID> players;
+    private final int targetCount;
+
     private boolean active;
+    private long lastTargetSpawnTimestamp;
 
     /**
      * Constructs {@code TargetGameService}.
@@ -57,12 +64,18 @@ public class TargetGameService implements Listener {
         this.plugin = plugin;
         this.world = world;
         this.gameUuid = UUID.randomUUID();
+        this.players = new HashSet<>();
+        this.targetCount = 5;
 
         this.npcRegistry = CitizensAPI.createAnonymousNPCRegistry(new MemoryNPCDataStore());
 
         this.targetLocations = List.of(
                 new Location(world, 32.5, 184, 144.5, -126F, 0F),
-                new Location(world, 27.5, 184, 138.5, -75F, 0F)
+                new Location(world, 27.5, 184, 138.5, -75F, 0F),
+                new Location(world, 29.5, 184, 126.5, -28F, 0F),
+                new Location(world, 28.5, 184, 131.5, -40F, 0F),
+                new Location(world, 35.5, 184, 125.5, -33F, 0F),
+                new Location(world, 22.5, 184, 126.5, -37, 0F)
         );
     }
 
@@ -76,25 +89,121 @@ public class TargetGameService implements Listener {
         }.runTaskTimer(this.plugin, 0, 0);
     }
 
-    public void tick() {
+    public @NonNull NPCRegistry registry() {
+        return this.npcRegistry;
+    }
 
+    public @NonNull JavaPlugin plugin() {
+        return this.plugin;
+    }
+
+    public @NonNull UUID uuid() {
+        return this.gameUuid;
+    }
+
+    public void tick() {
+        final long spawnDelta = System.currentTimeMillis() - this.lastTargetSpawnTimestamp;
+
+        if (spawnDelta >= 1000) {
+            loadTarget();
+        }
+
+        System.out.println(this.targets.size());
+    }
+
+    public boolean isPlaying(final @NonNull Player player) {
+        return this.players.contains(player.getUniqueId());
+    }
+
+    public boolean hasTarget(final @NonNull Location location) {
+        for (final @NonNull TargetInstance target : this.targets) {
+            if (target.location().equals(location)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void handlePlayerJoin(final @NonNull Player player) {
+        for (final @NonNull NPC npc : this.npcRegistry) {
+            if (npc.getEntity() instanceof Player playerNpc) {
+                player.showPlayer(this.plugin, playerNpc);
+            }
+        }
+        this.players.add(player.getUniqueId());
+
+        player.sendMessage(Component.text("Welcome to targets."));
+    }
+
+    public void handlePlayerLeave(final @NonNull Player player) {
+        for (final @NonNull NPC npc : this.npcRegistry) {
+            if (npc.getEntity() instanceof Player playerNpc) {
+                player.hidePlayer(this.plugin, playerNpc);
+            }
+        }
+        this.players.remove(player.getUniqueId());
+
+        player.sendMessage(Component.text("Thanks for playing targets."));
+    }
+
+    private void loadTarget() {
+        for (final @NonNull Location location : this.targetLocations) {
+            if (!hasTarget(location)) {
+                final @NonNull TargetInstance target = new TargetInstance(location, this);
+                this.targets.add(target);
+                this.lastTargetSpawnTimestamp = System.currentTimeMillis();
+                return;
+            }
+        }
     }
 
     @EventHandler
-    public void onTargetHit(final @NonNull NPCDamageByEntityEvent event) {
-        final @NonNull NPC npc = event.getNPC();
+    private void onRegionEntryOrExit(final @NonNull PlayerMoveEvent event) {
+        if (event.hasChangedBlock()) {
+            final @NonNull Player player = event.getPlayer();
+
+            if (locationService.inArea(player, REGION_ID) && !isPlaying(player)) {
+                handlePlayerJoin(player);
+                return;
+            }
+
+            if (!locationService.inArea(player, REGION_ID) && isPlaying(player)) {
+                handlePlayerLeave(player);
+                return;
+            }
+        }
+    }
+
+    @EventHandler
+    private void onTargetHit(final @NonNull EntityDamageByEntityEvent event) {
+        if (!event.getEntity().hasMetadata("NPC")) return;
+
+        final @NonNull NPC npc = this.registry().getNPC(event.getEntity());
 
         final @NonNull MetadataStore data = npc.data();
 
-        if (data.get("target", false)) {
-            if (data.has("game") && data.get("game").equals(this.gameUuid)) {
+        if (data.get("target", "false").equals("true")) {
+            final @Nullable String gameUuid = data.get("game", null);
+
+            if (gameUuid == null) {
+                return;
+            }
+
+            if (gameUuid.equals(this.gameUuid.toString())) {
+
                 final @NonNull Iterator<TargetInstance> iterator = this.targets.iterator();
 
                 while (iterator.hasNext()) {
                     final @NonNull TargetInstance target = iterator.next();
 
-                    if (target.uuid().equals(npc.getUniqueId())) {
-                        target.remove();
+                    if (target.npc().getUniqueId().equals(npc.getUniqueId())) {
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                target.remove();
+                            }
+                        }.runTaskLater(this.plugin, 1);
                         iterator.remove();
                     }
                 }
@@ -102,12 +211,16 @@ public class TargetGameService implements Listener {
         }
     }
 
-    public @NonNull NPCRegistry registry() {
-        return this.registry();
-    }
-
-    public @NonNull UUID uuid() {
-        return this.gameUuid;
+    /**
+     * Hides all active targets from the player.
+     *
+     * @param event the event
+     */
+    @EventHandler
+    public void onPlayerJoin(final @NonNull PlayerJoinEvent event) {
+        for (final @NonNull TargetInstance target : this.targets) {
+            event.getPlayer().hidePlayer(this.plugin, (Player) target.npc().getEntity());
+        }
     }
 
 }
--- bukkit/src/main/java/dev/kscott/bluelobby/games/targets/TargetInstance.java
+++ bukkit/src/main/java/dev/kscott/bluelobby/games/targets/TargetInstance.java
@@ -1,12 +1,11 @@
 package dev.kscott.bluelobby.games.targets;
 
 import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
-
-import java.util.UUID;
 
 public class TargetInstance {
 
@@ -30,12 +29,18 @@ public class TargetInstance {
         this.game = game;
 
         this.npc = this.game.registry().createNPC(EntityType.PLAYER, "Target");
-        this.npc.data().set("target", true);
-        this.npc.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN);
+        this.npc.data().set("target", "true");
+        this.npc.data().set("game", game.uuid().toString());
+        this.npc.setProtected(false);
+        this.npc.spawn(location);
+
+        for (final @NonNull Player player : Bukkit.getOnlinePlayers()) {
+            player.hidePlayer(game.plugin(), (Player) this.npc.getEntity());
+        }
     }
 
-    public @NonNull UUID uuid() {
-        return this.npc.getUniqueId();
+    public @NonNull NPC npc() {
+        return this.npc;
     }
 
     /**
@@ -53,6 +58,10 @@ public class TargetInstance {
     public void remove() {
         this.npc.despawn();
         this.game.registry().deregister(this.npc);
+    }
+
+    public @NonNull Location location() {
+        return this.location.clone();
     }
 
 }
